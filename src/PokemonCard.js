7afc8d9e0d68364b74abaafa0ec8ca6dac951a95
ArthSales <arthur.snascimento01@gmail.com>
2025-05-10 11:58:39 -0300

    Commit inicial, começando o projeto Pokedex

---
+++ src/PokemonCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokemonCard = () =>{
+    return(
+        <div>
+            This is the pokemon card page page.
+        </div>
+    )
+}
+
+export default PokemonCard;
---
+++ src/mockData.js
@@ -0,0 +1,169 @@
+export default{
+    "1": {
+        name: "bulbasaur",
+        id: 1,
+        species: {
+            name:"bulbasaur",
+            url:"https://pokeapi.co/api/v2/pokemon-species/1/"
+        },
+        weight:69,
+        height:7,
+        types: [
+            {
+                slot: 2,
+                type: {
+                    name: "poison",
+                    url:"https://pokeapi.co/api/v2/type/4/"
+                }
+            },
+            {
+                slot: 1,
+                type: {
+                    name:"grass",
+                    url:"https://pokeapi.co/api/v2/type/12/"
+                }
+            }
+        ],
+        sprites: {
+            back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+            back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png"
+        }
+    },
+    "2": {
+        name: "ivysaur",
+        id: 2,
+        species: {
+            name:"ivysaur",
+            url:"https://pokeapi.co/api/v2/pokemon-species/2/"
+        },
+        weight:130,
+        height:10,
+        types: [
+            {
+                slot: 2,
+                type: {
+                    name: "poison",
+                    url:"https://pokeapi.co/api/v2/type/4/"
+                }
+            },
+            {
+                slot: 1,
+                type: {
+                    name:"grass",
+                    url:"https://pokeapi.co/api/v2/type/12/"
+                }
+            }
+        ],
+        sprites: {
+            back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png",
+            back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/2.png"
+        }
+    },
+    "3": {
+        name: "venusaur",
+        id: 3,
+        species: {
+            name:"venusaur",
+            url:"https://pokeapi.co/api/v2/pokemon-species/2/"
+        },
+        weight:1000,
+        height:20,
+        types: [
+            {
+                slot: 2,
+                type: {
+                    name: "poison",
+                    url:"https://pokeapi.co/api/v2/type/4/"
+                }
+            },
+            {
+                slot: 1,
+                type: {
+                    name:"grass",
+                    url:"https://pokeapi.co/api/v2/type/12/"
+                }
+            }
+        ],
+        sprites: {
+            back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/3.png",
+            back_female:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/female/3.png",
+            back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/3.png"
+        }
+    },
+    "4": {
+        name: "charmander",
+        id: 4,
+        species: {
+            name:"charmander",
+            url:"https://pokeapi.co/api/v2/pokemon-species/4/"
+        },
+        weight:85,
+        height:6,
+        types: [
+            {
+                slot: 1,
+                type: {
+                    name:"fire",
+                    url:"https://pokeapi.co/api/v2/type/10/"
+                }
+            }
+        ],
+        sprites: {
+            back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png",
+            back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/4.png"
+        }
+    },
+    "5": {
+        name: "charmeleon",
+        id: 5,
+        species: {
+            name:"charmeleon",
+            url:"https://pokeapi.co/api/v2/pokemon-species/5/"
+        },
+        weight:85,
+        height:6,
+        types: [
+            {
+                slot: 1,
+                type: {
+                    name:"fire",
+                    url:"https://pokeapi.co/api/v2/type/10/"
+                }
+            }
+        ],
+        sprites: {
+            back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png",
+            back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/5.png"
+        }
+    },
+    "6": {
+        name: "charizard",
+        id: 6,
+        species: {
+            name:"charizard",
+            url:"https://pokeapi.co/api/v2/pokemon-species/6/"
+        },
+        weight:85,
+        height:6,
+        types: [
+            {
+                slot: 1,
+                type: {
+                    name:"fire",
+                    url:"https://pokeapi.co/api/v2/type/10/"
+                }
+            },
+            {
+                slot: 2,
+                type: {
+                    name:"flying",
+                    url:"https://pokeapi.co/api/v2/type/3/"
+                }
+            }
+        ],
+        sprites: {
+            back_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png",
+            back_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/6.png"
+        }
+    }
+}
